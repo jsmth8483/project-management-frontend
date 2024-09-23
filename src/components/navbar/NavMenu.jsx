8b82951a011eb67e4f3be027e9dcb4ef0fb43b12
James Smith <jameswsmith805@gmail.com>
2024-09-23 11:07:39 -0700

    implemented logout handler

--- src/components/navbar/NavMenu.jsx
+++ src/components/navbar/NavMenu.jsx
@@ -1,11 +1,31 @@
 import { Button, List, Navbar, Typography } from '@material-tailwind/react';
 import Logo from '../logo/Logo';
 import { useNavigate } from 'react-router-dom';
+import { backendConfig } from '../../config';
+
 const NavMenu = () => {
 	const navigate = useNavigate();
-	const handleLogout = () => {
-		navigate('/taskflow');
+
+	const handleLogout = async () => {
+		try {
+			const response = await fetch(
+				`${backendConfig.authService}/api/auth/logout`,
+				{
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				}
+			);
+
+			if (response.status === 204) {
+				navigate('/taskflow');
+			}
+		} catch (err) {
+			console.log('Error during logout: ' + err.message);
+		}
 	};
+
 	return (
 		<Navbar className='w-full max-w-full p-2 rounded-none border-b border-gray-200 shadow-sm text-blue-gray flex justify-between'>
 			<Logo />
